34c8b2ed1087e7a735db9c9c181185db186e6146
Evgenii <naterid@yandex.ru>
2022-08-03 16:16:00 +0700

    add a string with /'

---
+++ index.js
@@ -0,0 +1,7 @@
+// Экранирование буквенных кавычек в строках
+
+// Чтобы добавить кавычки в js код и они отобразились, т.е написать буквальную цитату. Нужно перед ними добавить косую черту /" 
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr); // => I am a "double quoted" string inside "double quotes".
+
