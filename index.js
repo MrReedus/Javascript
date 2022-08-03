95fdea1f35e84ef9d4e0ec7c344b4b4371d4d636
Evgenii <naterid@yandex.ru>
2022-08-03 16:34:14 +0700

    add special sumbols list

--- index.js
+++ index.js
@@ -4,4 +4,18 @@
 
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 console.log(myStr); // => I am a "double quoted" string inside "double quotes".
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>'; // двойные кавычки внутри одинарных
+
+/* 
+Специальные символы для того чтобы js вывел их на экран
+Код	Выход
+\'	одинарная кавычка
+\"	двойная кавычка
+\\	обратная косая черта
+\n	новая линия
+\r	возврат каретки
+\t	вкладка
+\b	граница слова
+\f	подача формы
+*/
 
