277c5e1d11d0dfa2d9ca970819fbbde00a6d215b
Evgenii <naterid@yandex.ru>
2022-08-09 13:59:42 +0700

    Использование объектов для поиска

--- index.js
+++ index.js
@@ -760,6 +760,26 @@ const myDog = {
 };
 delete myDog.tails;
 
+// Использование объектов для поиска 
+
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    const lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank"
+    };
+
+    result = lookup[val]
+
+    return result;
+}
+
 
 
 
