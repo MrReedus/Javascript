9af66850caee80d87fb808be28b7d72977d5cb7b
Evgenii <naterid@yandex.ru>
2022-08-09 16:16:51 +0700

    Доступ к вложенным массивам

--- index.js
+++ index.js
@@ -834,7 +834,32 @@ const myStorage = {
     }
 };
 
-const gloveBoxContents = myStorage.car.inside['glove box'];
+const gloveBoxContents = myStorage.car.inside['glove box']; //получаем значение maps
+
+
+//Доступ к вложенным массивам
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1]
+
 
 
 
