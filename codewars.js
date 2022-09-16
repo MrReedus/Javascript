5c0b31972d80446951019e7acd111067a61f2628
Evgenii <naterid@yandex.ru>
2022-09-16 23:42:04 +0700

    Позиция числа фибоначи

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-// Замена всех глассных в получаемй строке
+// Замена всех глассных в получаемой строке
 
 let disemvowel = str => str.replace(/[aeiou]/gi, '')
 
@@ -6,4 +6,21 @@ let disemvowel = str => str.replace(/[aeiou]/gi, '')
 
 let disemvowel = function (str) {
     return str.replace(/[aeiou]/gi, '')
-} 
+}
+
+
+// Позиция числа фыибоначи 
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 числа фибоначи в этой задаче 
+function nthFibo(n) {
+    let a = 0;
+    b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+
