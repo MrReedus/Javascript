f94902c588429968d09d09cb199d6bae26528d5c
Evgenii <naterid@yandex.ru>
2023-01-16 23:52:48 +0500

    task reverse number

--- index.js
+++ index.js
@@ -1814,6 +1814,24 @@ Promise.race([sleep(2000), sleep(5000)]).then(() => {
 }) // выполняет код когда первый пролмис выполнен
 
 
+// Задача на возвращение обратного числа 
+
+function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    const toString = n + '';
+    if (toString[toString.length - 1] === 0) {
+        toString.replace('0', '')
+    }
+    return +toString.split('')
+        .reverse()
+        .join('');
+}
+
+
+
+
 
 
 
