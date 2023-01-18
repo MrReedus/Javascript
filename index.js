81494ba1a87756a86c7f767cd1355d723e0b2b7d
Evgenii <naterid@yandex.ru>
2023-01-18 22:37:32 +0300

    str to camelCase

--- index.js
+++ index.js
@@ -1828,6 +1828,18 @@ function reverse(n) {
         .reverse()
         .join('');
 }
+// Перевод входной смтроки в camelCase
+
+function camelize(str) {
+    return str
+        .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+        .map(
+            // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+            // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        )
+        .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+}
 
 
 
